08ed48dc5c6905b1f8e90a7157e4c3496fd8545a
JonasHans <jonas.vanoenen@gmail.com>
2018-09-25 23:00:42 +0200

    buldings

--- src/store/index.js
+++ src/store/index.js
@@ -4,6 +4,7 @@ import Vuex from 'vuex'
 import settings from './modules/settings'
 import game from './modules/game'
 import resources from './modules/resources/'
+import buildings from './modules/buildings/'
 
 Vue.use(Vuex)
 
@@ -11,7 +12,8 @@ export default new Vuex.Store({
   modules: {
     settings,
     game,
-    resources
+    resources,
+    buildings
   },
   state: {
 
