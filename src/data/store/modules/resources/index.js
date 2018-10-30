2a6801b57e46fb09bab6ec6ce2d4caaf40fea9eb
JonasHans <jonas.vanoenen@gmail.com>
2018-10-30 21:45:16 +0100

    set incomes

--- src/data/store/modules/resources/index.js
+++ src/data/store/modules/resources/index.js
@@ -1,4 +1,5 @@
 import utils from '../../../../game/common/utils'
+import forEach from 'lodash.foreach'
 
 // TODO: Use same structures / utils as resources (static data, resourcesForAge, etc...)
 
@@ -9,11 +10,48 @@ export default {
       id: 'food',
       kind: 'resource',
       amount: 0,
-      base: 1
+      base: 1,
+      requirements: null
+    },
+    wood: {
+      id: 'wood',
+      kind: 'resource',
+      amount: 0,
+      base: 1,
+      requirements: {
+        buildings: {
+          cave: 5
+        }
+      }
+    },
+    stone: {
+      id: 'stone',
+      kind: 'resource',
+      amount: 0,
+      base: 1,
+      requirements: {
+        buildings: {
+          bonfire: 10,
+          cave: 20
+        }
+      }
+    },
+    gold: {
+      id: 'gold',
+      kind: 'resource',
+      amount: 0,
+      base: 1,
+      requirements: {
+        furnace: 1,
+        metalsmith: 1
+      }
     }
   },
   getters: {
     food: state => state.food,
+    wood: state => state.wood,
+    stone: state => state.stone,
+    gold: state => state.gold,
 
     resources: state => utils.getEntitiesByKind(state, 'resource'),
     resourcesDict: (state, getters) => utils.listToDict(getters.resources),
@@ -27,14 +65,20 @@ export default {
     // Should have "spend" action that sets new amounts based on cost opbject
 
     /**
-     * Click handler, maybe give a better name. Need sleep now :p
-     */
-    increment ({ getters, commit }, { type }) {
-      const resource = getters[type]
+    * Click handler, maybe give a better name. Need sleep now :p
+    */
+    increment ({ getters, commit }, { id }) {
+      const resource = getters[id]
       // TODO: Get modifiers from actual things
       const modifiers = { base: 0, multiplier: 1 }
       const newAmount = resource.amount + (resource.base + modifiers.base) * modifiers.multiplier
-      commit('setResource', { type, amount: newAmount })
+      commit('setResource', { id, amount: newAmount })
+    },
+    income ({ getters, commit }, { resources }) {
+      // Set new incomes for every resource
+      forEach(resources, (amount, id) => {
+        commit('setResource', { id, amount })
+      })
     }
   },
   mutations: {
